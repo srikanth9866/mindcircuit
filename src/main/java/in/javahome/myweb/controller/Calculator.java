2175583dfb02a628fc9774d1eb46aba95a76fd54
srikanth karre <93827375+srikanth9866@users.noreply.github.com>
2025-09-04 01:10:33 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -1,24 +1,15 @@
 package in.javahome.myweb.controller;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class CalculatorTest {
-
-    Calculator calc = new Calculator();
-
-    @Test
-    public void testAdd() {
-        assertEquals(5, calc.add(2, 3));
+public class Calculator {
+    public int add(int i, int j) {
+        return i + j;
     }
 
-    @Test
-    public void testMultiply() {
-        assertEquals(6, calc.multiply(2, 3));
+    public int multiply(int i, int j) {
+        return i * j;
     }
 
-    @Test
-    public void testSub() {
-        assertEquals(1, calc.sub(3, 2));
+    public int sub(int i, int j) {
+        return i - j;
     }
 }
